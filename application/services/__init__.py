6d726481f8a17d3572434a8d5139691b59f6fec8
linyi <linyiforce@gmail.com>
2026-04-01 07:15:37 +0800

    feat: add IndexingService for chapter indexing and search

--- application/services/__init__.py
+++ application/services/__init__.py
@@ -1,4 +1,5 @@
 """应用层服务"""
 from application.services.novel_service import NovelService
+from application.services.indexing_service import IndexingService
 
-__all__ = ["NovelService"]
+__all__ = ["NovelService", "IndexingService"]
